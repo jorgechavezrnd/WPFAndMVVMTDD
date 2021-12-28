b2e38816bea81552bd350201852cf60a91cf7186
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-27 20:16:06 -0400

    Add project, as class library, for Unit Tests with XUnit

---
+++ ThomasMathLib/ThomasMathLibTestsXUnit/CalculatorTestsXUnit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThomasMathLib;
+using Xunit;
+
+namespace ThomasMathLibTestsXUnit
+{
+    public class CalculatorTestsXUnit
+    {
+        [Fact]
+        public void AddShouldReturnSumOfValues()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
