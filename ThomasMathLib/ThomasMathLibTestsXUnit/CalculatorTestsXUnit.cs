d04a1db5e23b4367e63b61a9a904e0199f896549
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-27 20:20:23 -0400

    Add example for Parameterized Unit Tests with xUnit

--- ThomasMathLib/ThomasMathLibTestsXUnit/CalculatorTestsXUnit.cs
+++ ThomasMathLib/ThomasMathLibTestsXUnit/CalculatorTestsXUnit.cs
@@ -10,12 +10,15 @@ namespace ThomasMathLibTestsXUnit
 {
     public class CalculatorTestsXUnit
     {
-        [Fact]
-        public void AddShouldReturnSumOfValues()
+        [Theory]
+        [InlineData(5, 2, 3)]
+        [InlineData(10, 4, 6)]
+        [InlineData(2, 5, -3)]
+        public void AddShouldReturnSumOfValues(int expectedResult, int val1, int val2)
         {
             var calculator = new Calculator();
-            var result = calculator.Add(2, 3);
-            Assert.Equal(5, result);
+            var result = calculator.Add(val1, val2);
+            Assert.Equal(expectedResult, result);
         }
     }
 }
