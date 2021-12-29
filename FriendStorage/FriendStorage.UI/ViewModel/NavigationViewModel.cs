c547107ce9ca4a1a16f7a312628f02b5ea6e282a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-29 13:11:47 -0400

    Add one more test for NavigationViewModel and fix the Load method clearing the Friends observable collection before load the data

--- FriendStorage/FriendStorage.UI/ViewModel/NavigationViewModel.cs
+++ FriendStorage/FriendStorage.UI/ViewModel/NavigationViewModel.cs
@@ -16,6 +16,7 @@ namespace FriendStorage.UI.ViewModel
 
         public void Load()
         {
+            Friends.Clear();
             foreach (var friend in _dataProvider.GetAllFriends())
             {
                 Friends.Add(friend);
--- FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
+++ FriendStorage/FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -20,6 +20,25 @@ namespace FriendStorage.UITests.ViewModel
             viewModel.Load();
 
             Assert.Equal(2, viewModel.Friends.Count);
+
+            var friend = viewModel.Friends.SingleOrDefault(f => f.Id == 1);
+            Assert.NotNull(friend);
+            Assert.Equal("Julia", friend.FirstName);
+
+            friend = viewModel.Friends.SingleOrDefault(f => f.Id == 2);
+            Assert.NotNull(friend);
+            Assert.Equal("Thomas", friend.FirstName);
+        }
+
+        [Fact]
+        public void ShouldLoadFriendsOnlyOnce()
+        {
+            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+
+            viewModel.Load();
+            viewModel.Load();
+
+            Assert.Equal(2, viewModel.Friends.Count);
         }
     }
 
