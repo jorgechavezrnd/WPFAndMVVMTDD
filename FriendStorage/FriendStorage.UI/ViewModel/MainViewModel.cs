c2f2ad27ad515ed2c55f7bcda47bb9cea5908d67
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-30 19:03:39 -0400

    Refactor MainViewModel extracting some lines of code and creating the CreateAndLoadFriendEditViewModel function

--- FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
+++ FriendStorage/FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -35,10 +35,15 @@ namespace FriendStorage.UI.ViewModel
 
         private void OnAddFriendExecute(object obj)
         {
+            SelectedFriendEditViewModel = CreateAndLoadFriendEditViewModel(null);
+        }
+
+        private IFriendEditViewModel CreateAndLoadFriendEditViewModel(int? friendId)
+        {
             var friendEditVm = _friendEditVmCreator();
             FriendEditViewModels.Add(friendEditVm);
-            friendEditVm.Load(null);
-            SelectedFriendEditViewModel = friendEditVm;
+            friendEditVm.Load(friendId);
+            return friendEditVm;
         }
 
         private void OnOpenFriendEditView(int friendId)
@@ -46,9 +51,7 @@ namespace FriendStorage.UI.ViewModel
             var friendEditVm = FriendEditViewModels.SingleOrDefault(vm => vm.Friend.Id == friendId);
             if (friendEditVm == null)
             {
-                friendEditVm = _friendEditVmCreator();
-                FriendEditViewModels.Add(friendEditVm);
-                friendEditVm.Load(friendId);
+                friendEditVm = CreateAndLoadFriendEditViewModel(friendId);
             }
             SelectedFriendEditViewModel = friendEditVm;
         }
