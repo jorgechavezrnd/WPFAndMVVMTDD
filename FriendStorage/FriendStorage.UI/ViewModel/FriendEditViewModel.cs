85cbb7cac543f6ffbbed121020b7fec6108926c3
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-30 00:33:48 -0400

    Add tests for FriendEditViewModel without implementation of the logic yet

--- FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
+++ FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FriendStorage.Model;
+using FriendStorage.UI.DataProvider;
 
 namespace FriendStorage.UI.ViewModel
 {
@@ -10,6 +11,13 @@ namespace FriendStorage.UI.ViewModel
 
     public class FriendEditViewModel : ViewModelBase, IFriendEditViewModel
     {
+        private IFriendDataProvider @object;
+
+        public FriendEditViewModel(IFriendDataProvider @object)
+        {
+            this.@object = @object;
+        }
+
         public Friend Friend => throw new System.NotImplementedException();
 
         public void Load(int friendId)
