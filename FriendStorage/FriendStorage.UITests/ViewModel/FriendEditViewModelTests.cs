73c44e8c28adefb6b400a9c1ecaf4fbc6a5e693d
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-30 21:48:50 -0400

    Modify ShouldPublishFriendDeletedEventWhenDeleteCommandIsExecuted test using the ShowYesNoDialog logic implemented

--- FriendStorage/FriendStorage.UITests/ViewModel/FriendEditViewModelTests.cs
+++ FriendStorage/FriendStorage.UITests/ViewModel/FriendEditViewModelTests.cs
@@ -206,18 +206,29 @@ namespace FriendStorage.UITests.ViewModel
 
             _dataProviderMock.Verify(dp => dp.DeleteFriend(_friendId),
                 Times.Exactly(expectedDeleteFriendCalls));
+
             _messageDialogServiceMock.Verify(ds => ds.ShowYesNoDialog(It.IsAny<string>(),
                 It.IsAny<string>()), Times.Once);
         }
 
-        [Fact]
-        public void ShouldPublishFriendDeletedEventWhenDeleteCommandIsExecuted()
+        [Theory]
+        [InlineData(MessageDialogResult.Yes, 1)]
+        [InlineData(MessageDialogResult.No, 0)]
+        public void ShouldPublishFriendDeletedEventWhenDeleteCommandIsExecuted(
+            MessageDialogResult result, int expectedPublishCalls)
         {
             _viewModel.Load(_friendId);
 
+            _messageDialogServiceMock.Setup(ds => ds.ShowYesNoDialog(It.IsAny<string>(),
+                It.IsAny<string>())).Returns(result);
+
             _viewModel.DeleteCommand.Execute(null);
 
-            _friendDeletedEventMock.Verify(e => e.Publish(_friendId), Times.Once);
+            _friendDeletedEventMock.Verify(e => e.Publish(_friendId),
+                Times.Exactly(expectedPublishCalls));
+
+            _messageDialogServiceMock.Verify(ds => ds.ShowYesNoDialog(It.IsAny<string>(),
+                It.IsAny<string>()), Times.Once);
         }
     }
 }
