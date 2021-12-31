43b80fc3b278c113a6c7389af88b938e22c6a169
jorgechavezrnd <jorgechavezrnd@gmail.com>
2021-12-30 20:56:58 -0400

    Add implementation and tests for close the tab after delete

--- FriendStorage/FriendStorage.UITests/ViewModel/MainViewModelTests.cs
+++ FriendStorage/FriendStorage.UITests/ViewModel/MainViewModelTests.cs
@@ -15,6 +15,7 @@ namespace FriendStorage.UITests.ViewModel
     {
         private Mock<INavigationViewModel> _navigationViewModelMock;
         private OpenFriendEditViewEvent _openFriendEditViewEvent;
+        private FriendDeletedEvent _friendDeletedEvent;
         private Mock<IEventAggregator> _eventAggregatorMock;
         private MainViewModel _viewModel;
         private List<Mock<IFriendEditViewModel>> _friendEditViewModelMocks;
@@ -25,9 +26,13 @@ namespace FriendStorage.UITests.ViewModel
             _navigationViewModelMock = new Mock<INavigationViewModel>();
 
             _openFriendEditViewEvent = new OpenFriendEditViewEvent();
+            _friendDeletedEvent = new FriendDeletedEvent();
+
             _eventAggregatorMock = new Mock<IEventAggregator>();
             _eventAggregatorMock.Setup(ea => ea.GetEvent<OpenFriendEditViewEvent>())
                 .Returns(_openFriendEditViewEvent);
+            _eventAggregatorMock.Setup(ea => ea.GetEvent<FriendDeletedEvent>())
+                .Returns(_friendDeletedEvent);
 
             _viewModel = new MainViewModel(_navigationViewModelMock.Object,
                 CreateFriendEditViewModel, _eventAggregatorMock.Object);
@@ -111,6 +116,21 @@ namespace FriendStorage.UITests.ViewModel
             _viewModel.CloseFriendTabCommand.Execute(friendEditVm);
 
             Assert.Equal(0, _viewModel.FriendEditViewModels.Count);
+        }
+
+        [Fact]
+        public void ShouldRemoveFriendEditViewModelOnFriendDeletedEvent()
+        {
+            const int deletedFriendId = 7;
+
+            _openFriendEditViewEvent.Publish(deletedFriendId);
+            _openFriendEditViewEvent.Publish(8);
+            _openFriendEditViewEvent.Publish(9);
+
+            _friendDeletedEvent.Publish(deletedFriendId);
+
+            Assert.Equal(2, _viewModel.FriendEditViewModels.Count);
+            Assert.True(_viewModel.FriendEditViewModels.All(vm => vm.Friend.Id != deletedFriendId));
         }
     }
 }
